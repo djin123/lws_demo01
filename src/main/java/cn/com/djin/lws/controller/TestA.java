9ef9df46136d4a2cccedcd7539d09b2cc014d7fb
djin123 <502037675@qq.com>
2020-02-04 10:39:42 +0800

    第二次修改TestA.java文件，由组员3进行提交。。

--- src/main/java/cn/com/djin/lws/controller/TestA.java
+++ src/main/java/cn/com/djin/lws/controller/TestA.java
@@ -5,4 +5,8 @@ public class TestA {
     public static void main(String[] args) {
         System.out.println("第一次修改了此TestA类。。");
     }
+
+    public void test01(){
+        System.out.println("组员3写的测试test01方法。。");
+    }
 }
