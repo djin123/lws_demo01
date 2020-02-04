893f70536d051b75f4ff8bffe7c4890941b2f359
djin123 <502037675@qq.com>
2020-02-04 10:16:20 +0800

    创建了DemoC.java和DemoD.java文件，由组员2进行首次提交。。

---
+++ src/main/java/cn/com/djin/lws/controller/DemoC.java
@@ -0,0 +1,8 @@
+package cn.com.djin.lws.controller;
+
+public class DemoC {
+
+    public static void main(String[] args) {
+        System.out.println("创建了DemoC.java文件，由组员2进行首次提交。。");
+    }
+}
---
+++ src/main/java/cn/com/djin/lws/controller/DemoD.java
@@ -0,0 +1,8 @@
+package cn.com.djin.lws.controller;
+
+public class DemoD {
+
+    public static void main(String[] args) {
+        System.out.println("创建了DemoD.java文件，由组员2进行首次提交。。");
+    }
+}
